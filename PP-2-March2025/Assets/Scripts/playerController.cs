a53110f70135a0e217c200273cf23c6cde076053
Kylemay11 <kmskatemay@gmail.com>
2025-03-09 12:31:32 -0400

    Added a basic sprint

--- PP-2-March2025/Assets/Scripts/playerController.cs
+++ PP-2-March2025/Assets/Scripts/playerController.cs
@@ -4,22 +4,46 @@ using System;
 public class playerController : MonoBehaviour
 {
     [SerializeField] CharacterController controller;
-    [SerializeField] int speed;
 
-    Vector3 moveDir;
+    [Header("Movment Settings")]
+    [Range(1, 5)][SerializeField] private int walkSpeed;
+    [Range(1, 5)][SerializeField] private int sprintMult;
+
+    private Vector3 moveDir;
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
     
     // Update is called once per frame
     void Update()
     {
-        moveDir = (Input.GetAxis("Horizontal") * transform.right) + (Input.GetAxis("Vertical") * transform.forward);
-        controller.Move(moveDir * speed * Time.deltaTime);
+        HandleMovement();
 
+        sprint();
+
+    }
+
+    void HandleMovement()
+    {
+        moveDir = (Input.GetAxis("Horizontal") * transform.right) + (Input.GetAxis("Vertical") * transform.forward);
+        controller.Move(moveDir * walkSpeed * Time.deltaTime);
+    }
+
+    void sprint()
+    {
+        if(Input.GetButtonDown("Sprint"))
+        {
+            walkSpeed *= sprintMult;
+        }
+        else if(Input.GetButtonUp("Sprint"))
+        {
+            walkSpeed /= sprintMult;
+        }
     }
 
     private void Move(Vector3 vector3)
