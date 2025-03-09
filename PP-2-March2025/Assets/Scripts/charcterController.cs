251adbfc24416b8a4699c5233ac0b03473d97ea7
Kylemay11 <kmskatemay@gmail.com>
2025-03-08 19:33:39 -0500

    Fixed .gitignore and removed unwanted tracked files

--- PP-2-March2025/Assets/Scripts/charcterController.cs
+++ PP-2-March2025/Assets/Scripts/charcterController.cs
@@ -15,6 +15,7 @@ public class charcterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveDir = new Vector3(Input.GetAxis("Horizantal"), 0, Input.GetAxis("Vertical"));
+        moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.position += moveDir * speed * Time.deltaTime;
     }
 }
