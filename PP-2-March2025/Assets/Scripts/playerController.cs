d53d8c9444a069e3b4785653aec6f84661aa070f
Kylemay11 <kmskatemay@gmail.com>
2025-03-11 11:25:21 -0400

    Refactor playerController, Improved sprint system, added dynamic stamina handling

--- PP-2-March2025/Assets/Scripts/playerController.cs
+++ PP-2-March2025/Assets/Scripts/playerController.cs
@@ -14,18 +14,21 @@ public enum PlayerState
 
 public class playerController : MonoBehaviour, IDamage
 {
-    [SerializeField] LayerMask ignoreMask;
-    [SerializeField] CharacterController controller;
+    [Header("References")]
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private LayerMask ignoreMask;
+    [SerializeField] private TMP_Text speedMeter;
+    [SerializeField] private Transform playerCamera;
 
     private PlayerState currentState;
 
     [Header("Player Stats")]
-    [SerializeField] public int HP;
+    [SerializeField] private int maxHP;
+    [SerializeField] private int currentHP;
 
-    [Header("Movment Settings")]
-    [SerializeField] private TMP_Text speedMeter;
-    [Range(1f, 10f)][SerializeField] private float walkSpeed;
-    [Range(1f, 5f)][SerializeField] private float sprintMult;
+    [Header("Movement Settings")]
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float sprintMultiplier;
 
     [Header("Stamina Settings")]
     [SerializeField] private float maxStamina;
@@ -33,68 +36,100 @@ public class playerController : MonoBehaviour, IDamage
     [SerializeField] private float staminaDrainRate;
     [SerializeField] private float staminaRegainRate;
     [SerializeField] private float staminaRegainDelay;
-    [SerializeField] private float staminaSprintThreshold;
-    private float staminaRegenTimer;
-    private bool canSprint;
+    [SerializeField] private float slideStaminaDrainMultiplier;
 
     [Header("Slide Settings")]
     [SerializeField] private float slideDuration;
-    [SerializeField] private float slideBoost;
+    [SerializeField] private float slideSpeedBoost;
 
     [Header("Jump Settings")]
-    [Range(1, 25)][SerializeField] private int jumpSpeed;
-    [Range(8f, 45f)][SerializeField] private float gravity;
-    [Range(1, 3)][SerializeField] private int maxJumps;
-    [SerializeField] private bool isGrounded;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float gravity;
+    [SerializeField] private int maxJumps;
 
     [Header("Crouch Settings")]
-    [SerializeField] private int crouchSpeed;
+    [SerializeField] private float crouchSpeed;
     [SerializeField] private Vector3 crouchColliderSize;
     [SerializeField] private Vector3 normalColliderSize;
     [SerializeField] private float crouchCameraHeight;
     [SerializeField] private float normalCameraHeight;
-    [SerializeField] private Transform playerCamera;
 
     private Vector3 moveDir;
     private Vector3 velocity;
     private int jumpCount;
     private float currentSpeed;
-    float HPOrig;
+    private float staminaRegenTimer;
+    private bool staminaFullyDrained;
+    private bool canSprint;
+    private bool isGrounded;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        HPOrig = HP;
-        updatePlayerUI();
-        currentState = PlayerState.Idle;
+        currentHP = maxHP;
+        currentStamina = maxStamina;
         currentSpeed = walkSpeed;
+        currentState = PlayerState.Idle;
 
         if (playerCamera != null)
-            playerCamera.localPosition = new Vector3(playerCamera.localPosition.x, normalCameraHeight, playerCamera.localPosition.z);
+            UpdateCameraHeight(normalCameraHeight);
+
         controller.height = normalColliderSize.y;
-        controller.center = new Vector3(0, normalColliderSize.y / 14f, 0);
+        controller.center = new Vector3(0, normalColliderSize.y / 16f, 0);
+        updatePlayerUI();
     }
 
-    // Update is called once per frame
     void Update()
     {
         HandleMovement();
-        Sprint();
+        HandleSprint();
         HandleCrouch();
         HandleStamina();
-        updateSpeed();
+        UpdateSpeedUI();
     }
 
-    private void updateSpeed()
+    private void HandleMovement()
     {
-        if (speedMeter != null)
+        if (controller.isGrounded)
         {
-            float flatSpeed = new Vector3(controller.velocity.x, 0, controller.velocity.z).magnitude;
-            speedMeter.text = $"Speed: {currentSpeed:F2}";
+            isGrounded = true;
+            velocity.y = 0;
+            jumpCount = 0;
+
+            if (currentState != PlayerState.Sprinting && currentState != PlayerState.Crouching && currentState != PlayerState.Sliding)
+            {
+                currentState = PlayerState.Walking;
+                currentSpeed = walkSpeed;
+            }
+
+            if (currentState == PlayerState.Crouching)
+                currentSpeed = crouchSpeed;
+        }
+
+        moveDir = (Input.GetAxis("Horizontal") * transform.right) + (Input.GetAxis("Vertical") * transform.forward);
+        controller.Move(moveDir * currentSpeed * Time.deltaTime);
+
+        HandleJump();
+
+        velocity.y -= gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void HandleSprint()
+    {
+        if (Input.GetButtonDown("Sprint") && isGrounded && canSprint)
+        {
+            staminaRegenTimer = 0f;
+            currentState = PlayerState.Sprinting;
+            currentSpeed = walkSpeed * sprintMultiplier;
+        }
+        else if (Input.GetButtonUp("Sprint") && currentState == PlayerState.Sprinting)
+        {
+            currentState = PlayerState.Walking;
+            currentSpeed = walkSpeed;
         }
     }
-    void HandleCrouch()
+
+    private void HandleCrouch()
     {
         if (Input.GetButtonDown("Crouch"))
         {
@@ -116,140 +151,33 @@ public class playerController : MonoBehaviour, IDamage
             ExitCrouch();
         }
     }
-    void EnterCrouch()
+
+    private void EnterCrouch()
     {
         currentState = PlayerState.Crouching;
         currentSpeed = crouchSpeed;
         controller.height = crouchColliderSize.y;
-        controller.center = new Vector3(0, crouchColliderSize.y / 14f, 0);
-        if (playerCamera != null)
-            playerCamera.localPosition = new Vector3(playerCamera.localPosition.x, crouchCameraHeight, playerCamera.localPosition.z);
-    }
-    void EnterSlide()
-    {
-        currentState = PlayerState.Sliding;
-        controller.height = crouchColliderSize.y;
-        controller.center = new Vector3(0, crouchColliderSize.y / 6f, 0);
-        if (playerCamera != null)
-            playerCamera.localPosition = new Vector3(playerCamera.localPosition.x, crouchCameraHeight, playerCamera.localPosition.z);
-
-        currentSpeed = walkSpeed * sprintMult * slideBoost;
-        StartCoroutine(SlideTimer());
+        controller.center = new Vector3(0, crouchColliderSize.y / 2f, 0);
+        UpdateCameraHeight(crouchCameraHeight);
     }
 
-    void ExitCrouch()
+    private void ExitCrouch()
     {
         currentState = PlayerState.Walking;
         currentSpeed = walkSpeed;
         controller.height = normalColliderSize.y;
-        controller.center = new Vector3(0, normalColliderSize.y / 14f, 0);
-        if (playerCamera != null)
-            playerCamera.localPosition = new Vector3(playerCamera.localPosition.x, normalCameraHeight, playerCamera.localPosition.z);
-    }
-    void HandleMovement()
-    {
-        if (controller.isGrounded)
-        {
-            isGrounded = true;
-            jumpCount = 0;
-            velocity = Vector3.zero;
-
-            if (IsSprintingHeld() && currentState != PlayerState.Crouching && currentState != PlayerState.Sliding)
-            {
-                currentState = PlayerState.Sprinting;
-                currentSpeed = walkSpeed * sprintMult;
-            }
-            else if (currentState != PlayerState.Crouching && currentState != PlayerState.Sliding)
-            {
-                currentState = PlayerState.Walking;
-                currentSpeed = walkSpeed;
-            }
-
-            if (currentState == PlayerState.Crouching)
-                currentSpeed = crouchSpeed;
-        }
-        moveDir = (Input.GetAxis("Horizontal") * transform.right) + (Input.GetAxis("Vertical") * transform.forward);
-        controller.Move(moveDir * currentSpeed * Time.deltaTime);
-
-        Jump();
-
-        controller.Move(velocity * Time.deltaTime);
-        velocity.y -= gravity * Time.deltaTime;
-    }
-    void Sprint()
-    {
-        if (Input.GetButtonDown("Sprint") && isGrounded)
-        {
-            currentState = PlayerState.Sprinting;
-            currentSpeed = walkSpeed * sprintMult;
-        }
-        else if (Input.GetButtonUp("Sprint") && currentState == PlayerState.Sprinting)
-        {
-            currentState = PlayerState.Walking;
-            currentSpeed = walkSpeed;
-        }
-    }
-    void HandleStamina()
-    {
-        if (currentState == PlayerState.Sprinting)
-        {
-            currentStamina -= staminaDrainRate * Time.deltaTime;
-            currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
-
-            if(currentStamina <= 0)
-            {
-                currentState = PlayerState.Walking;
-                currentSpeed = walkSpeed;
-            }
-        }
-        else if(currentStamina < maxStamina)
-        {
-            currentStamina += staminaRegainRate + Time.deltaTime;
-            currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
-        }
-        updatePlayerUI();
+        controller.center = new Vector3(0, normalColliderSize.y / 16f, 0);
+        UpdateCameraHeight(normalCameraHeight);
     }
 
-    void ExitSlide()
+    private void EnterSlide()
     {
-        if (Input.GetButton("Crouch"))
-        {
-            EnterCrouch();
-        }
-        else if (IsSprintingHeld())
-        {
-            currentState = PlayerState.Sprinting;
-            currentSpeed = walkSpeed * sprintMult;
-        }
-        else
-        {
-            currentState = PlayerState.Walking;
-            currentSpeed = walkSpeed;
-        }
-
-        controller.height = normalColliderSize.y;
-        controller.center = new Vector3(0, normalColliderSize.y / 6f, 0);
-        if (playerCamera != null)
-            playerCamera.localPosition = new Vector3(playerCamera.localPosition.x, normalCameraHeight, playerCamera.localPosition.z);
-    }
-
-    void Jump()
-    {
-        if (Input.GetButtonDown("Jump") && jumpCount < maxJumps)
-        {
-            isGrounded = false;
-            jumpCount++;
-            velocity.y = jumpSpeed;
-        }
-    }
-    bool IsSprintingHeld()
-    {
-        return Input.GetButton("Sprint");
-    }
-
-    private void Move(Vector3 vector3)
-    {
-        throw new NotImplementedException();
+        currentState = PlayerState.Sliding;
+        currentSpeed = walkSpeed * sprintMultiplier * slideSpeedBoost;
+        controller.height = crouchColliderSize.y;
+        controller.center = new Vector3(0, crouchColliderSize.y / 2f, 0);
+        UpdateCameraHeight(crouchCameraHeight);
+        StartCoroutine(SlideTimer());
     }
 
     private IEnumerator SlideTimer()
@@ -258,18 +186,117 @@ public class playerController : MonoBehaviour, IDamage
         ExitSlide();
     }
 
-    public void takeDamage(int amount)
+    private void ExitSlide()
     {
-        HP -= amount;
-        updatePlayerUI();
-        StartCoroutine(flashDamageScreen());
-        if (HP <= 0)
+        if (Input.GetButton("Crouch"))
         {
-            gameManager.instance.youLose();
+            EnterCrouch();
+        }
+        else if (Input.GetButton("Sprint"))
+        {
+            currentState = PlayerState.Sprinting;
+            currentSpeed = walkSpeed * sprintMultiplier;
+        }
+        else
+        {
+            currentState = PlayerState.Walking;
+            currentSpeed = walkSpeed;
+        }
+
+        controller.height = normalColliderSize.y;
+        controller.center = new Vector3(0, normalColliderSize.y / 16f, 0);
+        UpdateCameraHeight(normalCameraHeight);
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount < maxJumps)
+        {
+            velocity.y = jumpForce;
+            jumpCount++;
         }
     }
 
-    IEnumerator flashDamageScreen()
+    private void HandleStamina()
+    {
+        if (currentState == PlayerState.Sprinting)
+        {
+            currentStamina -= staminaDrainRate * Time.deltaTime;
+            currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
+
+            if (currentStamina <= 0)
+            {
+                staminaFullyDrained = true;
+                canSprint = false;
+                currentState = PlayerState.Walking;
+                currentSpeed = walkSpeed;
+            }
+            staminaRegenTimer = 0f;
+        }
+
+        if (currentState == PlayerState.Sliding)
+        {
+            currentStamina -= staminaDrainRate * slideStaminaDrainMultiplier * Time.deltaTime;
+            currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
+            staminaRegenTimer = 0f;
+        }
+
+        if (currentState != PlayerState.Sprinting && currentState != PlayerState.Sliding)
+        {
+            if (staminaFullyDrained)
+            {
+                if (staminaRegenTimer < staminaRegainDelay)
+                {
+                    staminaRegenTimer += Time.deltaTime;
+                }
+                else
+                {
+                    RegainStamina();
+                }
+            }
+            else
+            {
+                RegainStamina();
+            }
+        }
+    }
+
+    private void RegainStamina()
+    {
+        if (staminaFullyDrained && currentStamina > 0)
+            staminaFullyDrained = false;
+
+        currentStamina += staminaRegainRate * Time.deltaTime;
+        currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
+
+        if (currentStamina > 0)
+            canSprint = true;
+    }
+
+    private void UpdateCameraHeight(float height)
+    {
+        if (playerCamera != null)
+            playerCamera.localPosition = new Vector3(playerCamera.localPosition.x, height, playerCamera.localPosition.z);
+    }
+
+    private void UpdateSpeedUI()
+    {
+        if (speedMeter != null)
+            speedMeter.text = $"Speed: {currentSpeed:F2}";
+        gameManager.instance.playerStaminaBar.fillAmount = currentStamina / maxStamina;
+    }
+
+    public void takeDamage(int amount)
+    {
+        currentHP -= amount;
+        updatePlayerUI();
+        StartCoroutine(DamageFlash());
+
+        if (currentHP <= 0)
+            gameManager.instance.youLose();
+    }
+
+    private IEnumerator DamageFlash()
     {
         gameManager.instance.playerDamageScreen.SetActive(true);
         yield return new WaitForSeconds(0.1f);
@@ -278,7 +305,6 @@ public class playerController : MonoBehaviour, IDamage
 
     public void updatePlayerUI()
     {
-        gameManager.instance.playerHPBar.fillAmount = (float)HP / HPOrig;
-        gameManager.instance.playerStaminaBar.fillAmount = currentStamina / maxStamina;
+        gameManager.instance.playerHPBar.fillAmount = (float)currentHP / maxHP;
     }
 }
