566ebce61ab1da254d575e2a64c823e871de2815
John Acevedo <166403326+TheMonarchOfShadows@users.noreply.github.com>
2025-03-11 23:12:30 -0400

    created the boss and ist stuff.

---
+++ PP-2-March2025/Assets/Scripts/BossAI.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossAI : MonoBehaviour
+{
+    [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Animator anim;
+
+    [SerializeField] int HP;
+    [SerializeField] int faceTargetSpeed;
+    [SerializeField] int animTranSpeed;
+
+    [SerializeField] Transform shootPos;
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootRate;
+
+    float shootTimer;
+
+    Vector3 playerDir;
+
+    bool playerInRange;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameManager.instance.updateGameGoal(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shootTimer += Time.deltaTime;
+        
+       
+        playerDir = gameManager.instance.player.transform.position - transform.position;
+        agent.SetDestination(gameManager.instance.player.transform.position);
+        
+        if (shootTimer >= shootRate)
+        {
+            shoot();
+        }
+        
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            faceTarget();
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, playerDir.y, playerDir.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
+    }
+
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+        StartCoroutine(flashRed());
+
+        agent.SetDestination(gameManager.instance.player.transform.position);
+        
+        if (HP <= 0)
+        {
+            gameManager.instance.updateGameGoal(-1);
+            Destroy(gameObject);
+        }
+    }
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = Color.white;
+    }
+
+    void shoot()
+    {
+        shootTimer = 0;
+        Instantiate(bullet, shootPos.position, transform.rotation);
+    }
+}
