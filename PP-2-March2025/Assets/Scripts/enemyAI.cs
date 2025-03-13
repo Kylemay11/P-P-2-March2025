fa9f9a8b1986669c2378eb1d4a086c0de542a429
Jacob Cotham <JACotham@student.fullsail.edu>
2025-03-13 15:53:18 -0400

    Enemy chasing player tested

--- PP-2-March2025/Assets/Scripts/enemyAI.cs
+++ PP-2-March2025/Assets/Scripts/enemyAI.cs
@@ -3,17 +3,40 @@ using UnityEngine.AI;
 
 public class enemyAI : MonoBehaviour
 {
-    public NavMeshAgent agent;
+    // [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+    // [SerializeField] Animator anim;
+
+    [SerializeField] int HP;
+    [SerializeField] int faceTargetSpeed;
+    // [SerializeField] int animTranSpeed;
+
+    [SerializeField] Transform attackPOS;
+    //[SerializeField] GameObject rangeAttack;
+    [SerializeField] float attackRate;
+    [SerializeField] float sightRange, attackRange;
+
+    float attackTimer;
+    Vector3 playerDir;
+    bool playerInSightRange, playerInAttackRange; 
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        attackTimer += Time.deltaTime;
+
+        playerDir = gameManager.instance.player.transform.position - transform.position; // always know
+        agent.SetDestination(gameManager.instance.player.transform.position);
+
+
     }
 }
