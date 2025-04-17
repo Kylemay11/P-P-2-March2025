daccbf64c996038ece8c6de15073da6595ed29b2
Cloud7z <yongchangsitu@gmail.com>
2025-04-17 14:18:46 -0400

    Aim Down Sights added, updated player prefab

---
+++ PP-2-March2025/Assets/Scripts/Aim Down Sights.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+public class ADS : MonoBehaviour
+{
+    [Header("Main Settings")]
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private Transform weapon;
+    [SerializeField] private KeyCode adsKey = KeyCode.Mouse1;
+
+    [Header("ADS Settings")]
+    [SerializeField] private float adsFOV;
+    [SerializeField] private float normalFOV;
+    [SerializeField] private Vector3 adsPosition;
+    [SerializeField] private float transitionSpeed;
+
+    private Vector3 originalWeaponPosition;
+    private bool isAiming;
+
+    private void Start()
+    {
+        // Store initial weapon position
+        originalWeaponPosition = weapon.localPosition;
+
+        // Set initial FOV
+        playerCamera.fieldOfView = normalFOV;
+    }
+
+    private void Update()
+    {
+        // Toggle ADS when key is pressed/released
+        if (Input.GetKeyDown(adsKey)) isAiming = true;
+        if (Input.GetKeyUp(adsKey)) isAiming = false;
+
+        // Apply ADS effects
+        if (isAiming)
+        {
+            AimDownSights();
+        }
+        else
+        {
+            ReturnToHip();
+        }
+    }
+
+    private void AimDownSights()
+    {
+        // Smoothly transition camera FOV
+        playerCamera.fieldOfView = Mathf.Lerp(
+            playerCamera.fieldOfView,
+            adsFOV,
+            transitionSpeed * Time.deltaTime
+        );
+
+        // Move weapon to ADS position
+        weapon.localPosition = Vector3.Lerp(
+            weapon.localPosition,
+            adsPosition,
+            transitionSpeed * Time.deltaTime
+        );
+    }
+
+    private void ReturnToHip()
+    {
+        // Return camera FOV to normal
+        playerCamera.fieldOfView = Mathf.Lerp(
+            playerCamera.fieldOfView,
+            normalFOV,
+            transitionSpeed * Time.deltaTime
+        );
+
+        // Return weapon to original position
+        weapon.localPosition = Vector3.Lerp(
+            weapon.localPosition,
+            originalWeaponPosition,
+            transitionSpeed * Time.deltaTime
+        );
+    }
+
+    // Public access to check if aiming
+    public bool IsAiming => isAiming;
+}
