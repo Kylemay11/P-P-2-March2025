5afd3df62e7c975f9b0793f8e1c0863ff543f01b
Jacob Cotham <JACotham@student.fullsail.edu>
2025-04-17 12:31:51 -0400

    ammo settings changing every play

--- PP-2-March2025/Assets/Scripts/weaponStats.cs
+++ PP-2-March2025/Assets/Scripts/weaponStats.cs
@@ -9,7 +9,7 @@ public class weaponStats : ScriptableObject
     [Range(5, 1000)] public int wepDist;
     [Range(0.1f, 2)] public float wepRate;
     public int ammoCur;
-    [Range(1, 500)] public int ammoMax;
+    [Range(1, 500)] public int magCapacity;
     [Range(10, 5000)] public int totalAmmo;
     [Range(0.1f, 5)] public float reloadTime;
 
@@ -20,7 +20,7 @@ public class weaponStats : ScriptableObject
 
     public void Reload()
     {
-        int ammoNeeded = ammoMax - ammoCur;
+        int ammoNeeded = magCapacity - ammoCur;
         // checks reserve to see if you can fully reload
         int ammoToAdd = Mathf.Min(ammoNeeded, totalAmmo);
 
