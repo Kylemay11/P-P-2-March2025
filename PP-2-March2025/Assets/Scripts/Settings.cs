ce45234c9ef842847e3a27fca1e7d0b5486c8208
Derick Hayes <hderick126@gmail.com>
2025-04-18 08:11:22 -0400

    made the audio slider for the settings

--- PP-2-March2025/Assets/Scripts/Settings.cs
+++ PP-2-March2025/Assets/Scripts/Settings.cs
@@ -1,17 +1,75 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using TMPro;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.Rendering;
 
 public class Settings : MonoBehaviour
 {
     public AudioMixer audiomix;
 
+    public TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    public TextMeshProUGUI FpsText;
+
+    private float pollingTime = 1f;
+    private float time;
+    private int frameCount;
+
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " X " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResIndex;
+        resolutionDropdown.RefreshShownValue();
+        
+    }
     public void setAudio(float vol)
     {
         audiomix.SetFloat("volume", vol);
     }
 
+    public void setRes(int resIndex)
+    {
+        Resolution resolution = resolutions[resIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
     public void setQuality(int qual)
     {
 
+    }
+    public void fpsCounter()
+    {
+        time += Time.deltaTime;
+
+        frameCount++;
+
+        if (time >= pollingTime)
+        {
+            int frameRate = Mathf.RoundToInt(frameCount / time);
+            FpsText.text = frameRate.ToString() + " FPS";
+
+            time -= pollingTime;
+            frameCount = 0;
+        }
     }
 }
