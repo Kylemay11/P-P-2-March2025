4c174895b37b11f17afd1d320c1f1d4faff796f1
John Acevedo <166403326+TheMonarchOfShadows@users.noreply.github.com>
2025-04-19 21:47:22 -0400

    added audio

--- PP-2-March2025/Assets/Scripts/audioForObjects.cs
+++ PP-2-March2025/Assets/Scripts/audioForObjects.cs
@@ -4,16 +4,25 @@ using System.Collections.Generic;
 
 public class audioForObjects : MonoBehaviour
 {
-    public AudioSource source;
-    public AudioClip clip;
-    public AudioClip gunShot;
+    private float nextPlayTime = 0f;
+    private float minTimeBetweenClips = 3f; // Minimum time in seconds between audio clip playbacks
+    private float maxTimeBetweenClips = 10f; // Maximum time in seconds between audio clip playbacks
 
+    [Header("--- Audio ---")]
+    [SerializeField] AudioSource aud;
+    [SerializeField] AudioClip[] audClips;
+    [Range(0, 1)][SerializeField] float audClipsVol;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+
+    if (Time.time >= nextPlayTime)
         {
-            source.PlayOneShot(clip);
+            // Play a random audio clip from the audClips array at the specified volume
+            aud.PlayOneShot(audClips[Random.Range(0, audClips.Length)], audClipsVol);
+
+            // Calculate the next time to play an audio clip
+            nextPlayTime = Time.time + Random.Range(minTimeBetweenClips, maxTimeBetweenClips);
         }
     }
 }
